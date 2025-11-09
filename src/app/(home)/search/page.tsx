370dc93f5d39273de4e58fa62110b717b36c7e89
Noh_yg <skyt852@gmail.com>
2025-11-09 22:11:12 +0900

    ✨ section 영역 추가

--- src/app/(home)/search/page.tsx
+++ src/app/(home)/search/page.tsx
@@ -1,7 +1,17 @@
+import { Search, SendHorizonal } from "lucide-react";
+
 export default function Page() {
   return (
     <>
-      <h1>Page</h1>
+      <section className="max-w-2xl mx-auto">
+        <form className="bg-white border border-[#F6F6F8] rounded-xl shadow">
+          <Search size={20} />
+          <input type="text" placeholder="검색하기..." />
+          <button type="submit" className="cursor-pointer">
+            <SendHorizonal size={20} />
+          </button>
+        </form>
+      </section>
     </>
   );
 }
